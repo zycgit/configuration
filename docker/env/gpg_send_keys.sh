c84e4c46005d1c9b3c72d8cc3bf01c98875b3f5c
zyc <zyc@hasor.net>
2015-09-02 17:48:04 +0800

    1.调试shell 脚本

--- docker/env/gpg_send_keys.sh
+++ docker/env/gpg_send_keys.sh
@@ -6,10 +6,10 @@ function sendKeys(){
     line=`echo $line | cut -d / -f 2 | cut -d " " -f 1`
     echo "check gpg key $line in server[hkp://pool.sks-keyservers.net]..."
     str=`gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys $line 2>&1 | grep "Total number" | cut -d : -f 3`
-    if [[ $str =~ "0" ]]; then
+    if [[ $str = "0" ]]; then
       echo "push to server..."
       gpg --keyserver hkp://pool.sks-keyservers.net --send-keys $line
-    elif [[ $str =~ "1" ]]; then
+    elif [[ $str = "1" ]]; then
       echo "the key $line has been push." 
     else
       echo "the key $line has error."
@@ -24,9 +24,9 @@ function checkKeys(){
     line=`echo $line | cut -d / -f 2 | cut -d " " -f 1`
     echo "check gpg key $line in server[hkp://pool.sks-keyservers.net]..."
     str=`gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys $line 2>&1 | grep "Total number" | cut -d : -f 3`
-    if [[ $str =~ "1" ]]; then
+    if [[ $str = "1" ]]; then
       return 1
-    elif [[ $str =~ "0" ]]; then
+    elif [[ $str = "0" ]]; then
       result=0
     fi
   done
